964b2099ccb56990b651c05adb38a00e0faca97d
mateo zarate <mateozaratefw@gmail.com>
2025-06-16 14:01:00 -0300

    ADD: added apple thing

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,28 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+  async headers() {
+    return [
+      {
+        source: "/apple-app-site-association",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+        ],
+      },
+      {
+        source: "/.well-known/apple-app-site-association",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-export default nextConfig
+export default nextConfig;
